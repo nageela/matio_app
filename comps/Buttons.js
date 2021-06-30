d0a3c12b423b97cf80590c13cf7d78cb9f4f4da4
kaarunyainparaj <kaarunya@cerebriam.co.uk>
2021-06-30 13:09:05 +0100

    commit

--- comps/Buttons.js
+++ comps/Buttons.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import React, { useState, Component } from "react";
+import React, { useState, setState, Component } from 'react';
+import Eventhandler from '../comps/Eventhandler';
 
 class Buttons extends Component {
 
@@ -18,39 +19,56 @@ class Buttons extends Component {
         }
       }
     eventhandler(name) {
-            switch(name){
-                case name == 'Interview':
+         
+                if (name == "Interview"){
+                  return (  
                     <Image 
                         src = '/images/interview_branch.png'
                         width = {100}
                         height = {100}
                         />
-                    
-                case 'Presentation':
-                    <Image 
-                        src = '/images/presentation_branch.png'
-                        width = {100}
-                        height = {100}
+                  )
+                }
+                else if(name == "Presentation"){
+                    return (
+                        <Image 
+                            src = '/images/presentation_branch.png'
+                            width = {100}
+                            height = {100}
                         />
-                case 'Vlog':
-                    <Image 
-                        src =  '/images/vlog_branch.png'
-                        width = {100}
-                        height = {100}
+                    )
+                }
+                else if(name == "Vlog"){
+                    return (
+                        <Image 
+                            src =  '/images/vlog_branch.png'
+                            width = {100}
+                            height = {100}
                         />
-                case 'Demo':
-                    <Image 
-                        src = '/images/demo_branch.png'
-                        width = {100}
-                        height = {100}
+                    )
+                }
+                else if(name == 'Demo') {
+                    return (
+                        <Image 
+                            src = '/images/demo_branch.png'
+                            width = {100}
+                            height = {100}
                         />
+                    )
+                }
         }
-    }
+        handleClick() {
+            this.setState({
+              clicked: true
+            });
+          }
+    
       render() {
         return (  
             <div class = "w-full xs:text-lg sm:text-xl md:text-2xl xs:mt-4 md:mt-5 lg:mt-10 grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 xs:gap-2 sm:gap-4 md:gap-6 xl:gap-10 px-8 xxl:px-20"> 
                 {this.state.buttonName.map((name, index) => (
-                        <button onClick = {this.eventhandler(name)}> 
+                        <button onClick = {this.handleClick}>
+                            {this.state.clicked ? <Eventhandler /> : null}
                             {<Image 
                                 src = {this.state.path[index]}
                                 width = {100}
